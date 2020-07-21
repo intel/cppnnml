743668e738fe679f463f6d3835d7081e85057281
dmcleran <daniel.mcleran@intel.com>
2020-07-21 14:23:09 -0600

    Move neural network into its own file for easier size parsing.

--- examples/xor/xor.cpp
+++ examples/xor/xor.cpp
@@ -21,62 +21,17 @@
 */
 
 #include <iostream>
-#include <cstdlib>
 #include <ctime>
 #include <fstream>
 #include <string>
 
 #include "constants.hpp"
-#include "qformat.hpp"
-#include "activationFunctions.hpp"
-#include "fixedPointTransferFunctions.hpp"
-#include "neuralnet.hpp"
 #include "nnproperties.hpp"
 
-// Q-Format value type
-static constexpr size_t NUMBER_OF_FIXED_BITS = 8;
-static constexpr size_t NUMBER_OF_FRACTIONAL_BITS = 8;
-typedef tinymind::QValue<NUMBER_OF_FIXED_BITS, NUMBER_OF_FRACTIONAL_BITS, true> ValueType;
-// typedef the underlying full-width representation type
-typedef typename ValueType::FullWidthValueType FullWidthValueType;
-// Neural network architecture
-static constexpr size_t NUMBER_OF_INPUTS = 2;
-static constexpr size_t NUMBER_OF_HIDDEN_LAYERS = 1;
-static constexpr size_t NUMBER_OF_NEURONS_PER_HIDDEN_LAYER = 3;
-static constexpr size_t NUMBER_OF_OUTPUTS = 1;
+#include "xornet.h"
 
 #define TRAINING_ITERATIONS 2000
 #define NUM_SAMPLES_AVG_ERROR 20
-
-// Random number generator
-struct RandomNumberGenerator
-{
-    static ValueType generateRandomWeight()
-    {
-        // Generate a random number between -1..1 in the Q Format full width type
-        const FullWidthValueType weight = (rand() % 
-                                                    (tinymind::Constants<ValueType>::one().getValue() +
-                                                     tinymind::Constants<ValueType>::one().getValue() -
-                                                     tinymind::Constants<ValueType>::negativeOne().getValue())) +
-                                                     tinymind::Constants<ValueType>::negativeOne().getValue();
-
-        return weight;
-    }
-};
-
-// Typedef of transfer functions for the fixed-point neural network
-typedef tinymind::FixedPointTransferFunctions<  ValueType,
-                                                RandomNumberGenerator,
-                                                tinymind::TanhActivationPolicy<ValueType>,
-                                                tinymind::TanhActivationPolicy<ValueType>> TransferFunctionsType;
-
-// typedef the neural network itself
-typedef tinymind::MultilayerPerceptron< ValueType,
-                                        NUMBER_OF_INPUTS,
-                                        NUMBER_OF_HIDDEN_LAYERS,
-                                        NUMBER_OF_NEURONS_PER_HIDDEN_LAYER,
-                                        NUMBER_OF_OUTPUTS,
-                                        TransferFunctionsType> NeuralNetworkType;
 
 static void generateXorTrainingValue(ValueType& x, ValueType& y, ValueType& z)
 {
@@ -89,7 +44,7 @@ static void generateXorTrainingValue(ValueType& x, ValueType& y, ValueType& z)
     z = ValueType(result, 0);
 }
 
-static NeuralNetworkType testNeuralNet;
+extern NeuralNetworkType testNeuralNet;
 
 int main(const int argc, char *argv[])
 {
