f5a3f13a4bbea6866f44a176fd407919651f0df7
Dan McLeran <daniel.mcleran@intel.com>
2021-04-13 15:45:47 -0700

    Remove need for ignore 4309 on MSVC.

--- cpp/qformat.hpp
+++ cpp/qformat.hpp
@@ -64,7 +64,7 @@ namespace tinymind {
     struct SignExtender
     {
         static constexpr T SignBitMask = static_cast<T>(1 << (NumberOfFixedBits + NumberOfFractionalBits - 1));
-        static constexpr T SignExtensionBits = static_cast<T>(UINT64_MAX << (NumberOfFixedBits + NumberOfFractionalBits));
+        static constexpr T SignExtensionBits = static_cast<T>((~(static_cast<size_t>(SignBitMask) - 1)) ^ SignBitMask);
 
         static void signExtend(T& value)
         {
